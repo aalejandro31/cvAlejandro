020f08c5a13da32138e1d0b3ef9a94ba81a1b50d
aalejandro31 <cvalex110@gmail.com>
2024-05-08 18:48:23 +0200

    Practica 2 tema 4 entornos.

--- src/Practica2_Tema4/PROYECTO.java
+++ src/Practica2_Tema4/PROYECTO.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class PROYECTO {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.println("Introduce tu edad para decirte si eres mayor de edad o no");
+        System.out.println("introduce tu edad para decirte si eres mayor de edad o no");
         int edad= sc.nextInt();
         if(edad>=18){
             System.out.println("¡eres mayor de edad!");
-        }else{
+        }else {
             System.out.println("¡eres menor de edad!");
         }
     }
